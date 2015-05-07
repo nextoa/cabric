3ee89d5ec55bf4464a16c2630dd5c03eb53f6afb
Breeze.kay <dev@kbonez.net>
2015-05-07 19:36:01 +0800

    imporve generate bree app code

--- cabric/tpl/web/app.py
+++ cabric/tpl/web/app.py
@@ -13,7 +13,6 @@ import bree.web
 import tornado.ioloop
 import tornado.web
 
-
 from demo import settings
 from demo.handlers import handlers
 
@@ -26,6 +25,7 @@ if __name__ == "__main__":
         '',
         '',
         'Options:',
+        ('--env:', 'explicit define settings'),
         ('--debug', 'debug flag'),
         ('--help', 'print help document', '-h'),
     ))
@@ -39,13 +39,21 @@ if __name__ == "__main__":
 
     print("[tornado]:demo start, bind port {}, begin at: {}.".format(port, datetime.now()))
 
+    current_env = a.options.get('--env') or 'pro'
+
+    if hasattr(settings, current_env):
+        base_settings = getattr(settings, current_env)
+    else:
+        raise Exception("Missing settings:`{}' in settings.py".format(current_env))
+
     settings_debug = None
     if debug:
+        print("[tornado]:service_notice default mode:{}.".format(current_env))
         print("[tornado]:demo debug mode enabled.")
         settings_debug = settings.dev
         pass
 
-    default_settings = bree.web.build_settings(settings.pro, __file__, debug=settings_debug)
+    default_settings = bree.web.build_settings(base_settings, __file__, debug=settings_debug)
     bree.settings = default_settings
 
     application = tornado.web.Application(handlers, **default_settings)
